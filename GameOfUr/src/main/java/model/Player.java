bed9e045a23dd0a8f918d5ff73931c09ea18a941
ximenaGdur <68168666+ximenaGdur@users.noreply.github.com>
2022-12-04 01:39:43 -0600

    Implementing base classes for abstract class Player. These have not been tested yet.

--- GameOfUr/src/main/java/model/Player.java
+++ GameOfUr/src/main/java/model/Player.java
@@ -15,26 +15,87 @@ import java.util.function.Supplier;
  * @author Jimena Gdur.
  * @param <PieceType> Piece's child class
  */
-public class Player<PieceType extends Piece> extends GameObject {
-    /**
-     * Stores all of the player's pieces.
-     */
-    protected ArrayList<PieceType> pieces;
+public abstract class Player<PieceType extends Piece> extends GameObject {
     /**
      * Player's chosen color.
      */
     protected Color playerColor;
+    /**
+     * Player's given name.
+     */
+    protected String playerName;
+    /**
+     * Player's current score.
+     */
+    protected int playerScore;
+    /**
+     * Player's inventory which contains InventoryItems.
+     */
+    //protected Inventory playerInventory;
+    /**
+     * Amount of pieces player has.
+     */
+    protected int piecesAmount;
+    /**
+     * Stores all of the player's pieces.
+     */
+    protected ArrayList<PieceType> playerPieces;
     
     /**
      * Creates a new Player with an array of pieces.
-     * @param supplier Supplier class that contains instance of piece's child
+     * @param supplier Supplier class that contains instance of piece's child.
+     * @param amountPieces Amount of pieces the player has.
+     * @param chosenColor Color player chose on respective screen.
+     * @param name Player's name.
     */
-    Player(Supplier<PieceType> supplier, int amountPieces, Color chosenColor) {
-        pieces = new ArrayList<>(amountPieces);
-        for (int pieceIndex = 0; pieceIndex < amountPieces; pieceIndex++) {
-            pieces.set(pieceIndex, supplier.get());
+    public Player(Supplier<PieceType> supplier, int amountPieces, Color chosenColor, String name) {
+        this.piecesAmount = amountPieces;
+        this.playerColor = chosenColor;
+        this.playerName = name;
+        initializePiecesArray(supplier);
+    }
+    /**
+     * Initializes player array that contains al their pieces.
+     * @param supplier Supplier class that contains instance of piece's child.
+    */
+    private void initializePiecesArray(Supplier<PieceType> supplier){
+        this.playerPieces = new ArrayList<>(this.piecesAmount);
+        for (int pieceIndex = 0; pieceIndex < this.piecesAmount; pieceIndex++) {
+            this.playerPieces.set(pieceIndex, supplier.get());
         }
     }
+    /**
+     * Returns player's color.
+     * @return Player's color.
+     */
+    public Color getColor() {
+        return this.playerColor;
+    }
+    /**
+     * Returns player's name.
+     * @return Player's name.
+     */
+    public String getPlayerName() {
+        return this.playerName;
+    }
+    /**
+     * Returns player's score.
+     * @return Player's score.
+     */
+    public int getPlayerScore() {
+        return this.playerScore;
+    }
+    /**
+     * Modifies player's score.
+     * This method is abstract.
+     */
+    public abstract void modifyPlayerScore();
+    /**
+     * Consults player for a piece.
+     * This method is abstract.
+     * @return
+     */
+    public abstract PieceType getAvailablePiece();
     @Override
     public String toString() {
         String string = "";
